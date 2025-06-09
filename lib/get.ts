dfbbe8d0edd2ca1246148bdb3e92bd1d0dadb0a0
Michael <87752300+michi-onl@users.noreply.github.com>
2025-06-10 01:22:54 +0200

    feat: update CSS for standalone mode and enhance API fetching with revalidation

--- lib/get.ts
+++ lib/get.ts
@@ -3,6 +3,7 @@ import { siteConfig } from "@/config/site";
 export async function getAPI({ url }: { url: string }) {
   try {
     const res = await fetch(`https://api.helldivers2.dev/api${url}`, {
+      next: { revalidate: 3600 },
       headers: {
         "X-Super-Client": siteConfig.x_super.client,
         "X-Super-Contact": siteConfig.x_super.contact,
