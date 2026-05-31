5e9deae187e122e1e32cfce25d1d1ef3d9d754fb
michi-onl <87752300+michi-onl@users.noreply.github.com>
2026-05-31 14:53:44 +0200

    feat: add statistics section component for merged home page

---
+++ components/widgets/merged/statistics-section.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/container";
+import Statistics from "@/components/widgets/statistics/statistics";
+
+export default function StatisticsSection() {
+  return (
+    <section id="statistics">
+      <Container title="Statistics">
+        <Statistics />
+      </Container>
+    </section>
+  );
+}
